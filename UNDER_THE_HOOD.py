45711ad6c67df3a9b33d0a33582deab26faadc39
Dharshini Ravikumar <dharshinimm2004@gmail.com>
2025-01-12 09:34:21 +0530

    Add files via upload

---
+++ UNDER_THE_HOOD.py
@@ -0,0 +1,17 @@
+UNDER THE HOOD 
+
+head = {'value':7,
+        'next':{'value':3,
+                'next':{'value':22,
+                        'next':{'value':11,
+                                'next':None}}}}
+print(head['value'])
+print(head['next']['value'])
+print(head['next']['next']['value'])
+print(head['next']['next']['next']['value'])
+_____________________________________________________________________
+OUTPUT:
+7
+3
+22
+11
