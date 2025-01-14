9c55942d5607f3a7ce4dde22e6fa101db1ec1482
Dharshini Ravikumar <dharshinimm2004@gmail.com>
2025-01-14 12:44:28 +0530

    Add files via upload

---
+++ remove_duplicates_nodes.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append_node(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value, end='->' if temp.next is not None else '\n')
+            temp = temp.next
+
+    def remove_duplicates(self):
+        current =self.head
+        while current and current.next:
+            if current.value==current.next.value:
+                current.next=current.next.next
+            else:
+                current=current.next
+        return self.head
+    
+
+def create_linked_list():
+    values = input("Enter the elements of the linked list separated by spaces: ").split()
+    li = LinkedList()
+    for i in values:
+        li.append_node(int(i))
+    return li
+
+if __name__ == "__main__":
+    li = create_linked_list()
+    li.remove_duplicates()
+    li.print_list()
